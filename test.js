23f767d7d88b2abfa8ff1f69f2af3878d0322f78
Alonso Alarcon <155941412+alxxjandro@users.noreply.github.com>
2025-04-10 19:01:31 -0600

    test

--- test.js
+++
@@ -1,3 +0,0 @@
-const response = await fetch(´https://super-duper-succotash-695qrpjpw6wr354j4-3000.app.github.dev/departamentos/${asd}´);
-const departments = await response.json();
-console.log(departments);
